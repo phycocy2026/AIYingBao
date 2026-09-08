e04b2fe7a2a2873141eac0567b40d0ab07fc1033
phycocy2026 <phycocy@gmail.com>
2026-09-08 12:27:58 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aiyingbao-pwa-v1-1';
+const CACHE = 'aiyingbao-pwa-v1-1-1';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', e => {
   self.skipWaiting();
